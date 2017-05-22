5009aa1438e86007de5403e829483c279f16fe70
Michael Pope <mpope.cr@gmail.com>
2017-05-21 19:00:13 -0700

    Add log capturer to supervision tree

--- lib/exlager_unit/application.ex
+++ lib/exlager_unit/application.ex
@@ -5,8 +5,11 @@ defmodule LagerUnit.Application do
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+    alias LagerUnit.LogCapturer
 
-    children = []
+    children = [
+      worker(LogCapturer, [])
+    ]
 
     opts = [strategy: :one_for_one, name: LagerUnit.Supervisor]
     Supervisor.start_link(children, opts)

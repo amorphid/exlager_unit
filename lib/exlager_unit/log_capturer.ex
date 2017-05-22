d1b9c7483d24d4e27e2bbe6628cd21af998cf418
Michael Pope <mpope.cr@gmail.com>
2017-05-22 06:51:54 -0700

    Add log capturing for tests

--- lib/exlager_unit/log_capturer.ex
+++ lib/exlager_unit/log_capturer.ex
@@ -9,6 +9,11 @@ defmodule LagerUnit.LogCapturer do
     GenServer.cast(__MODULE__, {:capture, msg})
   end
 
+  def register_test() do
+    test = self()
+    GenServer.call(__MODULE__, {:register_test, test})
+  end
+
   def start_link() do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
@@ -17,13 +22,18 @@ defmodule LagerUnit.LogCapturer do
   # Callbacks #
   #############
 
+  def handle_call({:register_test, test}, _from, state) do
+    new_state = MapSet.put(state, test)
+    {:reply, :ok, new_state}
+  end
+
   def handle_cast({:capture, msg}, state) do
-    # TODO: handle messages
+    _ = Enum.map(state, fn pid -> send(pid, msg) end)
     {:noreply, state}
   end
 
   def init(_) do
-    state = %{}
+    state = MapSet.new()
     {:ok, state}
   end
 end
